7b57f1701035773cffc82fecbc80ae6c52dc4597
Henrique Alves <henriquea@gmail.com>
2015-10-14 17:43:35 +0100

    Pump project page

--- site/index.js
+++ site/index.js
@@ -1,4 +1,5 @@
 import './styles/normalize.css';
+import './styles/syntax.css';
 import './styles/skeleton.css';
 import './styles/styles.css';
 
@@ -10,24 +11,31 @@ const Container = ({children}) =>
 
 class Site extends Component {
   render() {
+    const usageJsMarkup = {__html: 'minigrid({ container: ".grid", item: ".grid__item" });'};
+    const responsivenessJsMarkup = {__html: 'window.addEventListener("resize", function(){\n\tminigrid({ container: ".grid", item: ".grid__item" });\n});'}
+    const animationCSSMarkup = {__html: '.grid__item {\n\topacity: 0;\n\ttransition: opacity .3s ease;\n}\n\n.grid__item--animate {\n\topacity: 1;\n}'}
+    const animateAdvancedMarkup = {__html: '/* styles.css */\n.grid__item {\n\ttransition-property: opacity, scale;\n\topacity: 0;\n\tscale: 0.95;\n}\n\n/* index.js */\nminigrid({\n\tcontainer: ".grid",\n\titem: ".grid__item",\n\tanimate: function(el, x, y, index) {\n\t\tdynamics.animate(el, {\n\t\t\topacity: 1,\n\t\t\tscale: 1,\n\t\t\ttransformX: x,\n\t\t\ttransformY: y\n\t\t}, {\n\t\t\ttype: dynamics.spring,\n\t\t\tfrequency: 200,\n\t\t\tfriction: 270,\n\t\t\tduration: 800\n\t\t});\n\t}\n});'}
     return (
       <Container>
 
-        <ul className="navbar">
-          <li><a href="#">GitHub</a></li>
-          <li><a href="#">npm</a></li>
-        </ul>
+        <header className="navbar">
+          <div className="vs">Minigrid <cite>v2.2.1</cite></div>
+          <div className="links">
+            <a href="#">GitHub</a>
+            <a href="#">npm</a>
+          </div>
+        </header>
 
-        <div className="docs-section hero">
+        <div className="section hero">
           <div className="row">
             <div className="twelve columns">
-              <h6>Minigrid</h6>
-              <p>Cascading grid layout without pain.</p>
+              <h4>Cascading grid layout without pain.</h4>
+              <a href="#" className="button button-primary">Download</a>
             </div>
           </div>
         </div>
 
-        <div className="docs-section">
+        <div className="section">
           <div className="row">
             <div className="twelve columns">
               <h6>When to use?</h6>
@@ -40,23 +48,116 @@ class Site extends Component {
           </div>
         </div>
 
-        <div className="docs-section">
+        <div className="section">
           <div className="row">
             <div className="twelve columns">
-              <h6>Install</h6>
-              <pre><code>npm install minigrid</code></pre>
+              <h6><a href="#install" name="install">Install</a></h6>
+              <pre><code className="prettyprint lang-bsh">$ npm install minigrid</code></pre>
             </div>
           </div>
         </div>
 
-        <div className="docs-section">
+        <div className="section">
           <div className="row">
             <div className="twelve columns">
-              <h6>Usage</h6>
-              <p>Hello</p>
+              <h6><a href="#usage" name="usage">Usage</a></h6>
+              <p>It works on a grid container with a group of grid items.</p>
+              <pre>
+                <code className="prettyprint lang-html">
+                  &lt;div class="grid"&gt;{'\n'}
+                    {'\t'}&lt;div class="grid__item"&gt;&lt;/div&gt;{'\n'}
+                    {'\t'}&lt;div class="grid__item"&gt;&lt;/div&gt;{'\n'}
+                    {'\t'}&lt;div class="grid__item"&gt;&lt;/div&gt;{'\n'}
+                  &lt;/div&gt;
+                </code>
+              </pre>
+              <p>Initialize it passing two arguments: the grid container and child items selector.</p>
+                <pre>
+                  <code className="prettyprint lang-js">
+                    <span dangerouslySetInnerHTML={usageJsMarkup} />
+                  </code>
+                </pre>
             </div>
           </div>
         </div>
+
+        <div className="section">
+          <div className="row">
+            <div className="twelve columns">
+              <h6><a href="#api" name="api">API</a></h6>
+              <pre><code className="prettyprint lang-js">minigrid(<cite>props</cite>)</code></pre>
+              <p>props is an object with the follow properties:</p>
+              <ul>
+                <li><code>container *</code></li>
+                <li><code>item *</code></li>
+                <li><code>gutter</code></li>
+                <li><code>animate(item, x, y, index)</code></li>
+                <li><code>done(nodeList)</code></li>
+              </ul>
+              <p><code className="no-style">* required</code></p>
+              <p><strong>container</strong> <code className="no-style">string or node</code></p>
+              <p>The element selector or element where your grid items sit.</p>
+
+              <p><strong>item</strong> <code className="no-style">string</code></p>
+              <p>The grid item element selector.</p>
+
+              <p><strong>gutter</strong> <code className="no-style">number</code></p>
+              <p>The space between items, the default is 6.</p>
+
+              <p><strong>animate</strong> <code className="no-style">function</code></p>
+              <p>It returns a function with the <code className="no-style">item</code>, <code className="no-style">x</code>, <code className="no-style">y</code> and <code className="no-style">index</code> parameters for each child item.</p>
+              <p>See <a href="#animation">animation</a> for more information.</p>
+              <p><strong>done</strong> <code className="no-style">function</code></p>
+              <p>Callback called after the grid is built. It returns the node
+              list of grid items.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="section">
+          <div className="row">
+            <div className="twelve columns">
+              <h6><a href="#responsiveness" name="responsiveness">Responsiveness</a></h6>
+              <p>minigrid is dead-simple and doesn't provide anything in-the-box but you can do:</p>
+              <pre>
+                <code className="prettyprint lang-js">
+                  <span dangerouslySetInnerHTML={responsivenessJsMarkup} />
+                </code>
+              </pre>
+              <p><a href="#" className="button">Demo</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="section">
+          <div className="row">
+            <div className="twelve columns">
+              <h6><a href="#animation" name="animation">Animation</a></h6>
+              <p>Minigrid provide one simple way to animate the child items with CSS only. Minigrid appends the <code>--animate</code> to your grid item class name.</p>
+              <pre>
+                <code className="prettyprint lang-css">
+                  <span dangerouslySetInnerHTML={animationCSSMarkup} />
+                </code>
+              </pre>
+              <p><a href="#" className="button">Demo</a></p>
+              <p>You may use the <code>animate</code> function to control it via JavaScript. The example bellow is using <a href="http://dynamicsjs.com/">Dynamics.js</a> library.</p>
+              <pre>
+                <code className="prettyprint lang-js">
+                  <span dangerouslySetInnerHTML={animateAdvancedMarkup} />
+                </code>
+              </pre>
+              <p><a href="#" className="button">Demo</a></p>
+            </div>
+          </div>
+        </div>
+
+        <footer className="foo">
+          <div className="vs">Minigrid <cite>v2.2.1</cite></div>
+          <div className="links">
+            <a href="#">GitHub</a>
+            <a href="#">npm</a>
+          </div>
+        </footer>
 
       </Container>
     );
