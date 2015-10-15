a62ea5c5cbbca2c88021b57d0e3c3d0e02e24363
Henrique Alves <henriquea@gmail.com>
2015-10-16 00:10:15 +0100

    Fix minigrid dist

--- site/webpack.config.minigrid.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var webpack = require('webpack');
-var config = require('./webpack.config.dist.js');
-
-config.devtool = null;
-config.entry = '../src/index.js';
-config.output.filename = 'minigrid.min.js';
-
-module.exports = config;
