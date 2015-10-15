d436957f7dd5684d34cf45a7835146665d2bb25d
Henrique Alves <henriquea@gmail.com>
2015-10-15 10:43:46 +0100

    Cleanup code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* @license minigrid v1.6.5 - minimal cascading grid layout http://alves.im/minigrid */
 (function (exports) {
 
   'use strict';
@@ -42,27 +41,38 @@
   function init(props) {
     var containerEle = props.container instanceof Node ?
       props.container : document.querySelector(props.container);
-    if (!containerEle) { return false; }
+
+    if (!containerEle) {
+      return false;
+    }
+
     containerEle.classList.add(containerEle.className.split(' ')[0] + '--loaded');
     loaded = true;
+
     var itemsNodeList = containerEle.querySelectorAll(props.item);
-    if (itemsNodeList.length === 0) { return false; }
+    if (itemsNodeList.length === 0) {
+      return false;
+    }
+
     var gutter = (
       typeof props.gutter === 'number' &&
       isFinite(props.gutter) &&
       Math.floor(props.gutter) === props.gutter
     ) ? props.gutter : 6;
     containerEle.style.width = '';
+
     var containerWidth = containerEle.getBoundingClientRect().width;
     var firstChildWidth = itemsNodeList[0].getBoundingClientRect().width + gutter;
     var cols = Math.max(Math.floor((containerWidth - gutter) / firstChildWidth), 1);
     var count = 0;
+
     containerWidth = (firstChildWidth * cols + gutter) + 'px';
     containerEle.style.width = containerWidth;
     containerEle.style.position = 'relative';
 
     var itemsGutter = [];
     var itemsPosX = [];
+
     for ( var g = 0 ; g < cols ; ++g ) {
       itemsPosX.push(g * firstChildWidth + gutter);
       itemsGutter.push(gutter);
@@ -76,18 +86,24 @@
         })
         .shift();
       itemIndex = itemsGutter.indexOf(itemIndex);
+
       var posX = itemsPosX[itemIndex];
       var posY = itemsGutter[itemIndex];
+
       item.style.position = 'absolute';
       item.classList.add(item.className.split(' ')[0] + '--animate');
+
       if (!props.animate && transformProp) {
         item.style[transformProp] = 'translate3D(' + posX + 'px,' + posY + 'px, 0)';
       }
+
       itemsGutter[itemIndex] += item.getBoundingClientRect().height + gutter;
       count = count + 1;
+
       if (props.animate) {
         return props.animate(item, posX, posY, count);
       }
+
     });
 
     var containerHeight = itemsGutter
