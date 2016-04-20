721f9d90041401ee7eb280636c291723064e8c5d
Henrique Alves <henriquea@gmail.com>
2016-04-20 22:43:51 +0100

    Pages

--- build.js
+++ build.js
@@ -1,9 +1,35 @@
 var fs = require('fs');
-var index = fs.readFileSync('./README.md', 'utf8');
-var template = fs.readFileSync('./template.ejs', 'utf8');
+var pkg = require('./package.json');
+var markdownFile = fs.readFileSync('./README.md', 'utf8');
+var templateFile = fs.readFileSync('./template.hbs', 'utf8');
+
+var Handlebars = require('handlebars');
+//
+function titleCase(str){
+  if (typeof str === 'undefined') return '';
+  return str.replace(/\w\S*/g, function(txt){
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+
+function extend(a, b) {
+  for (var key in b) {
+    if (b.hasOwnProperty(key)) {
+      a[key] = b[key];
+    }
+  }
+  return a;
+}
+
+var markdownTemplate = Handlebars.compile(markdownFile);
+pkg.homepage = '[' + titleCase(pkg.name) + '](' + pkg.homepage + ')';
+pkg.github = '[GitHub](' + pkg.repository.url + ')';
+pkg.twitter = '['+ pkg.author.twitter + '](https://twitter.com/' + pkg.author.twitter + ')';
+pkg.title = titleCase(pkg.name);
+pkg.keywords = pkg.keywords.join(',');
+var compiledMarkdown = markdownTemplate(pkg);
 
 var minify = require('html-minifier').minify;
-var ejs = require('ejs');
 var remark = require('remark');
 var html = require('remark-html');
 var yamlConfig = require('remark-yaml-config');
@@ -14,14 +40,15 @@ var doc = processor.process([
     '  commonmark: true',
     '---',
     '',
-    index
+    compiledMarkdown
 ].join('\n'));
 
 var data = { content: doc };
-var options = {};
-var rendered = ejs.render(template, data, options);
+var options = extend(data, pkg);
+var template = Handlebars.compile(templateFile);
+var compiledTemplate = template(options);
 
-var html = minify(rendered, {
+var html = minify(compiledTemplate, {
   collapseWhitespace: true
 });
 
